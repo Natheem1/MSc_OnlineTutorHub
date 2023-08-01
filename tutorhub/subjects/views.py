33a51f9498d0fd2f09517cfe281b174693415635
Natheem1 <124581559+Natheem1@users.noreply.github.com>
2023-08-01 19:09:15 +0100

    Check our Views & URL root is working
    created some dummy functions.  Tested and checked URLs and Views making sure they have been setup, organised and working correctly.

--- tutorhub/subjects/views.py
+++ tutorhub/subjects/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Used for testing function and URL routes setup
+from django.http import HttpResponse
+
+# Dummy view functions for testing URL routes and setup.
+
+def subjects(request):
+    return HttpResponse("Search all Subjects here ")
+
+
+def subject(request, pk):
+    return HttpResponse("Single Subject" + "  " + str(pk))
