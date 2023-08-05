c797a1e008a19ca7c959b775fff9dea8322d02c5
Natheem1 <124581559+Natheem1@users.noreply.github.com>
2023-08-05 11:38:37 +0100

    Template configuration
    setup templates folder in settings.py
    created a templates folder and files.
    Tested rendering of subjects page & single-subjects page & Added dummy text in the templates.

--- tutorhub/subjects/views.py
+++ tutorhub/subjects/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
+# from django.http import HttpResponse
 
-# Used for testing function and URL routes setup
-from django.http import HttpResponse
-
-# Dummy view functions for testing URL routes and setup.
 
 def subjects(request):
-    return HttpResponse("Search all Subjects here ")
+    return render(request, 'subjects.html')
 
 
 def subject(request, pk):
-    return HttpResponse("Single Subject" + "  " + str(pk))
+    return render(request, 'single-subject.html')
