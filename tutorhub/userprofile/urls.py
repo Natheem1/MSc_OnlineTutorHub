55f476d1d39a5febe8738c9461c1d5751b2762a6
Natheem1 <124581559+Natheem1@users.noreply.github.com>
2023-08-09 15:57:12 +0100

    further development of user profiles

--- tutorhub/userprofile/urls.py
+++ tutorhub/userprofile/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('tutor-profiles/', views.tutorProfiles, name="tutor-profiles"),
-    path('student-profiles', views.studentProfiles, name="student-profiles")
+    path('student-profiles', views.studentProfiles, name="student-profiles"),
+
+    path('tutor/<str:pk>', views.tutorProfile, name="tutor"),
+    path('student/<str:pk>', views.studentProfile, name="student"),
 ]
--- tutorhub/userprofile/views.py
+++ tutorhub/userprofile/views.py
@@ -7,8 +7,20 @@ def tutorProfiles(request):
     context = {'tutors': tutors}
     return render(request, 'userprofile/tutor-profiles.html', context)
 
+#SINGLE TUTOR PROFILE
+def tutorProfile(request,pk):
+    tutorObj = TutorProfile.objects.get(id=pk)
+    context = {'tutorObj': tutorObj}
+    return render(request, 'userprofile/single-tutor.html', context)
+
 #ALL STUDENT PROFILES 
 def studentProfiles(request):
     students = StudentProfile.objects.all()
     context = {'students': students}
     return render (request, 'userprofile/student-profiles.html', context)
+
+#SINGLE STUDENT PROFILE
+def studentProfile(request,pk):
+    studentObj = StudentProfile.objects.get(id=pk)
+    context = {'studentObj': studentObj}
+    return render(request, 'userprofile/single-student.html', context)
