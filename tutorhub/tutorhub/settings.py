bd9431292f0de06d8f3556a695c2b27cf5512ff3
Natheem1 <124581559+Natheem1@users.noreply.github.com>
2023-08-09 15:16:44 +0100

    Some Styling for core templates - main, nav & footer

--- tutorhub/tutorhub/settings.py
+++ tutorhub/tutorhub/settings.py
@@ -115,11 +115,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 AUTH_USER_MODEL = "users.NewUser"
 MEDIA_URL = "/images/"
 
-STATISFILES_DIRS = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
