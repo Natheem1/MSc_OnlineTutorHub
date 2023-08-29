09feb1c85e7a3876b605b6d1c85905e9b7ebcad3
Natheem1 <124581559+Natheem1@users.noreply.github.com>
2023-08-29 15:51:41 +0100

    Further Development of Video Lesson App
    Integrated the ZegoCloud Video Conferencing App into the video Lesson App, tested and working

--- tutorhub/videolesson/urls.py
+++ tutorhub/videolesson/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path 
 from .import views 
 
-urlpatterns = []
+urlpatterns = [
+    path('videodashboard/', views.VideoLessonDashboard, name='video-dashboard'),
+    path('joinlesson/', views.JoinLesson, name='join-lesson'),
+    
+    path('createlesson/', views.CreateVideoLesson, name='create-lesson'),
+]
--- tutorhub/videolesson/views.py
+++ tutorhub/videolesson/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# Video Lesson Main Dashboard 
+@login_required
+def VideoLessonDashboard(request):
+    tutor = request.user.tutorprofile 
+
+    context = {'tutor': tutor}
+    return render(request, 'videolesson/videodashboard.html', context)
+
+# Join Video Lesson 
+@login_required
+def JoinLesson(request):
+    
+    if request.method == 'POST':
+        roomID = request.POST.get('roomID')
+        return redirect("/createlesson?roomID=" + roomID)
+    
+    return render(request, 'videolesson/joinmeeting.html')
+
+
+# Video Lesson - ZegoCloud
+@login_required
+def CreateVideoLesson(request):
+    user = request.user
+
+    context = {'user':user}
+    return render(request, 'videolesson/createmeeting.html', context)
